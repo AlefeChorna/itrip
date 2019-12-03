d516fcf62eaa16a26e0b9c932707d16bc5b7fbe8
Alefe Chorna <alefechorna99@gmail.com>
2019-12-02 22:21:51 -0300

    first commit

---
+++ app/src/main/java/br/com/itrip/ui/activity/TripsActivity.java
@@ -0,0 +1,16 @@
+package br.com.itrip.ui.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.itrip.R;
+
+public class TripsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trips);
+    }
+}
